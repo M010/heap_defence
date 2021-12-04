c05be21201b3748938bda4873d39ed8c895eacd7
Филипп Евсеев <FVEvseev@sberbank.ru>
2021-12-04 03:36:32 +0300

    sad

--- heap_defence.c
+++ heap_defence.c
@@ -1,6 +1,8 @@
 //
 // Created by moh on 30.11.2021.
 //
+#define DEVELOP
+#ifdef DEVELOP
 #include "flipperzero-firmware/core/furi.h"
 #include "flipperzero-firmware/applications/gui/gui.h"
 #include "flipperzero-firmware/applications/input/input.h"
@@ -10,7 +12,16 @@
 #include <stdlib.h>
 #include <string.h>
 //#include <stdlib.h>
-
+#elifdef TEST
+#include "../flipperzero-firmware/core/furi.h"
+#include "../flipperzero-firmware/applications/gui/gui.h"
+#include "../flipperzero-firmware/applications/input/input.h"
+#include "../flipperzero-firmware/lib/STM32CubeWB/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h"
+#include "../flipperzero-firmware/applications/input/input.h"
+#include "../flipperzero-firmware/firmware/targets/f7/furi-hal/furi-hal-resources.h"
+#include <stdlib.h>
+#include <string.h>
+#endif
 #ifdef _MLX_
 //#include "minilibx/mlx.h"
 
@@ -32,7 +43,7 @@ typedef enum {
 #define X_FIELD_SIZE 12
 #define BOX_HEIGHT 10
 #define BOX_WIDTH 10
-#define TIMER_UPDATE_FREQ 4
+#define TIMER_UPDATE_FREQ 30
 //#define GENERATE_BOX(x) { x->field[0][rand() % X_FIELD_SIZE]; }
 
 typedef u_int8_t byte;
@@ -94,10 +105,6 @@ void game_state_destroy(GameState *game_state) {
 	free(game_state);
 }
 
-static Pixel convert_box_position(byte x, byte y) {
-	return (Pixel){.x = x * 10, y = y * 10};
-}
-
 static void generate_box(GameState const * game_state) {
 	memset(game_state->field[0] + rand() % X_FIELD_SIZE, 0xff, sizeof(Box));
 }
@@ -122,10 +129,18 @@ static void drop_box(GameState *game_state) {
 
 static void clear_rows(Box **field) {
 	int bottom_row = Y_FIELD_SIZE - 1;
-	if (!memchr(field[bottom_row], 0, X_FIELD_SIZE))
-		return;
-	memset(field[bottom_row], 0, X_FIELD_SIZE);
+	int flag = 0;
 	for (int y = bottom_row; y > 0; --y) {
+		for (int x = 0; x < X_FIELD_SIZE; ++x) {
+			if (field[x][y].state == 0) {
+				flag = 1;
+				break ;
+			}
+		}
+		if (flag == 1) {
+			continue;
+		}
+		memset(field[bottom_row], 0, X_FIELD_SIZE);
 		heap_swap(field + y, field + y - 1);
 	}
 }
@@ -140,9 +155,8 @@ static void heap_defense_render_callback(Canvas* const canvas, void* mutex) {
 	canvas_set_color(canvas, ColorBlack);
 	for (int y = 0; y < Y_FIELD_SIZE; ++y) {
 		for (int x = 0; x < X_FIELD_SIZE; ++x) {
-			Pixel top_left = convert_box_position(x, y);
 			if (game_state->field[y][x].state) {
-				canvas_draw_box(canvas, top_left.x, top_left.y, BOX_WIDTH, BOX_HEIGHT);
+				canvas_draw_box(canvas, x * BOX_HEIGHT, y * BOX_WIDTH, BOX_WIDTH, BOX_HEIGHT);
 			}
 		}
 	}
@@ -207,15 +221,15 @@ int main() {
 				default:
 					break;
     		}
-    	} else if (event.type == EventGameTick) {
+    	} /* else if (event.type == EventGameTick) {
     		/// apply logic
     		//move_person();
     		drop_box(game_state);
     		//drop_person();
     		clear_rows(game_state->field);
     		generate_box(game_state);
-    	}
-		release_mutex(&state_mutex, game_state);
+    	}*/
+    	release_mutex(&state_mutex, game_state);
     	view_port_update(view_port);
     }
 
